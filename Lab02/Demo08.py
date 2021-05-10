250c28c0f3a2f400eba8e073a028ec20b2c131f9
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2021-05-10 15:46:21 +0700

    [Basic] Review - String (cont.)

--- Lab02/Demo08.py
+++ Lab02/Demo08.py
@@ -3,8 +3,36 @@ String in python
 """
 from Module.Module08 import *
 
-def main():
 
+def main():
+    file_input = "Data/input.txt"
+    file_output = "Data/output.txt"
+
+    string = read_file(file_input)
+    print(string)
+    str_upper = uppercase(string)
+    print(str_upper)
+    str_lower = lowercase(str_upper)
+    print(str_lower)
+    str_upper = upper_first_letter(string)
+    print(str_upper)
+
+    sub_string = "in THE present AgE"
+    int_result = find_substring(str_upper, sub_string)
+    if int_result != -1:
+        print(f"Substring '{sub_string}' is started at {int_result}")
+    else:
+        print(f"Substring '{sub_string}' is not exist")
+
+    str_result = get_substring(str_upper, 100, 200)
+    print(str_result)
+
+    str_result = insert_break_line(str_upper)
+    print(str_result)
+    str_result = insert_break_line_with_number_per_line(str_upper, 100)
+    print(str_result)
+
+    count_word(str_upper, "in")
 
 if __name__ == "__main__":
     main()
--- Lab02/Module/Module08.py
+++ Lab02/Module/Module08.py
@@ -1,14 +1,150 @@
 # 1. Read content from text-file
+def read_file(file_name):
+    file = open(file_name, "r")
+    string = file.read()
+    file.close()
+    return string
+
+
 # 2. Write content to text-file
+def write_file(file_name, string):
+    file = open(file_name, "w")
+    file.write(string)
+    file.close()
+
+
 # 3. Uppercase all content
+def uppercase(string):
+    str_result = string.upper()
+    return str_result
+
+
 # 4. Lowercase all content
+def lowercase(string):
+    str_result = string.lower()
+    return str_result
+
+
 # 5. Uppercase with the character in head of sentence
+def upper_first_letter(string):
+    string.strip()  # remove white-space in head or end of sentence
+    str_result = ""
+    for i in range(0, len(string)):
+        if i == 0:
+            str_result += string[i].upper()
+        elif string[i - 1] == "." or string[i - 1] == "?" or string[i - 1] == "!":
+            str_result += string[i].upper()
+        elif string[i - 1] == " " and (string[i - 2] == "." or string[i - 2] == "?" or string[i - 2] == "!"):
+            str_result += string[i].upper()
+        else:
+            str_result += string[i]
+    else:
+        return str_result
+
+
 # 6. Find substring in content
+def find_substring(string, sub_string):
+    str = string.lower()
+    sub = sub_string.lower()
+
+    for i in range(0, len(str)):
+        flag = True
+        for j in range(0, len(sub)):
+            if str[i + j] != sub[j]:
+                flag = False
+                break
+        if flag:
+            return i
+    else:
+        return -1
+
+
 # 7. Get substring from start to end in content
+def get_substring(string, start, end):
+    str_result = ""
+    if start < 0 or start > end:
+        return str_result
+    elif len(string) < start or len(string) < end:
+        print("Length of string is invalid input between start and end index")
+        return str_result
+
+    for i in range(start, end):
+        str_result += string[i]
+    else:
+        return str_result
+
+
 # 8. Break-line: Insert break-line character (\n) after end of sentence
+def insert_break_line(string):
+    str_result = ""
+    for i in range(0, len(string)):
+        if string[i] == " " and (string[i - 1] == "." or string[i - 1] == "!" or string[i - 1] == "?"):
+            str_result += "\n"
+        else:
+            str_result += string[i]
+    else:
+        return str_result
+
+
 # 9. Enter a integer number is called N, Break-line maximum N character in line
+def insert_break_line_with_number_per_line(string, number):
+    arr_word = split_word(string)
+    str_result = ""
+    length = 0
+    for item in arr_word:
+        if length + len(item) > number:
+            str_result += "\n"
+            length = 0
+        str_result += item + " "
+        length += len(item)
+    return str_result
+
+
 # 10. Copy all content of source-file to dest-file
-# 11. Count the number of "I"
+def copy_file(src_file_name, des_file_name):
+    string = read_file(src_file_name)
+    write_file(des_file_name, string)
+
+
+# 11. Count the number of word "I"
+def count_word(string, word):
+    arr_word = split_word_fix(string)
+    count = 0
+    for item in arr_word:
+        if word.lower() == item.lower():
+            count += 1
+    else:
+        return count
+
+
 # 12. Split word (not duplicate)
 # 13. Statistic frequency for each word
 # 14. Filter the number in content
+# 15. Split word
+def split_word(string):
+    arr_word = []
+    word = ""
+    for i in range(0, len(string)):
+        if string[i] == " ":
+            arr_word.append(word)
+            word = ""
+        else:
+            word += string[i]
+    else:
+        arr_word.append(word)
+        return arr_word
+
+
+# 16. Split word without punctuation
+def split_word_fix(string):
+    arr_word = []
+    word = ""
+    for i in range(0, len(string)):
+        if string[i] == " ":
+            arr_word.append(word)
+            word = ""
+        elif string[i] != "." or string[i] != "," or string[i] != ";" or string[i] != "?" or string[i] != "!":
+            word += string[i]
+    else:
+        arr_word.append(word)
+        return arr_word
